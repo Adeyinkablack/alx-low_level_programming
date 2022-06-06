fe257da02bec0f6e45c80b41ec0e5a8f33e4fdf4
Adeyinkablack <sadeniji21@gamil.com>
2022-06-06 04:08:14 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints all possible combinations of two different digits,
- * in ascending order, separated by a comma followed by a space.
+ *        in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
@@ -19,11 +19,12 @@ int main(void)
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;
+
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	
+
 	putchar('\n');
 
 	return (0);
